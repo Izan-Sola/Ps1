7ac43d8514568cd65fb1b344dd2cd44948223d2f
Izan Sola Montesinos <83542827+Izan-Sola@users.noreply.github.com>
2025-09-25 13:59:19 +0200

    Add files via upload

--- DeleteDuplicatedFiles.ps1
+++ DeleteDuplicatedFiles.ps1
@@ -4,7 +4,7 @@ Set-Location C:\Users\2smra
 function askHost() {
 
   $winver = Read-Host "Which windows version are you using? ( 11 or 10 )"
-  Write-Output "All the duplicated files are going to be shown."
+  Write-Output "All the duplicatedList files are going to be shown."
   Write-Output ""
 
   if($winver -eq "11") { searchFile(".*\-\ copia\..*") }
@@ -18,33 +18,45 @@ function askHost() {
 
 Write-Output ""
 
-function DelOrNot ($regExpression) {
+function DelOrNot ($duplicatedList) {
                    $YesNo = Read-Host -Prompt "Do you want to delete all the duplicated files? (Y/N)"
 
-                   if ( ($YesNo -eq "N") -or ($YesNo -eq "n") ) { Write-Output "You can now close the powershell." }
-                   ElseIf ( ($YesNo -eq "Y") -or ($YesNo -eq "y") ) {
+                  if ($YesNo -eq "Y") {
+                    foreach ($duplicated in $duplicatedList) {
+                      Remove-Item $duplicated
+                    }
+                    Write-Output "All the duplicated files have been removed"
+                  } 
+                    Write-Output "You can now close the powershell."
 
-                           Get-ChildItem .\ -Recurse -File | 
-                           Where-Object { $_.Name -match $regExpression } |
-                           Remove-Item
+                  #  if ( ($YesNo -eq "N") -or ($YesNo -eq "n") ) { Write-Output "You can now close the powershell." }
+                  #  ElseIf ( ($YesNo -eq "Y") -or ($YesNo -eq "y") ) {
+
+                  #          Get-ChildItem .\ -Recurse -File | 
+                  #          Where-Object { $_.Name -match $regExpression } |
+                  #          Remove-Item
     
-                            Write-Output "All the duplicated files have been removed."
-                          }
+                  #           Write-Output "All the duplicatedList files have been removed."
+                  #         }
                     }
 
 function searchFile($regExpression) {
-foreach ($file in Get-ChildItem .\ -Recurse -File | 
-          Where-Object { $_.Name -match $regExpression }) {
 
-          Write-Output $file
+  $duplicatedList = @()
+foreach ($file in Get-ChildItem .\ -Recurse -File | 
+          Where-Object { $_.Name -match $regExpression } | Select-Object -ExpandProperty FullName) {
+
+          $duplicatedList+=$file
+         
         }
+          Write-Output $duplicatedList
 
          if([string]::IsNullOrEmpty($file)) {
              Write-Output "No files have been found."
              Write-Output ""
              Write-Output "You can now close the powershell."
            }
-           else { DelOrNot($regExpression)}
+           else { DelOrNot($duplicatedList)}
 }
 Write-Output ""
 askHost
