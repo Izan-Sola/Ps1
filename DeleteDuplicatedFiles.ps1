f6afdd53ef4710440eb62084009ae07f73580534
Izan Sola Montesinos <83542827+Izan-Sola@users.noreply.github.com>
2025-09-25 12:19:45 +0200

    Add files via upload
    now uses regex and works for both windows 10 and 11

--- DeleteDuplicatedFiles.ps1
+++ DeleteDuplicatedFiles.ps1
@@ -1,29 +1,40 @@
 
 Set-Location C:\Users\2smra
 
-Write-Output "All the duplicated files with their directories are going to be shown."
+function askHost() {
+
+  $winver = Read-Host "Which windows version are you using? ( 11 or 10 )"
+  Write-Output "All the duplicated files are going to be shown."
+  Write-Output ""
+
+  if($winver -eq "11") { searchFile(".*\-\ copia\..*") }
+  elseif ($winver -eq "10") { searchFile(".*\([0-9]?[0-9]\)\.[A-z,0-9]{2,4}") }
+  else {
+      Write-Output "Invalid windows version"
+      askHost
+ }
+
+}
+
 Write-Output ""
 
-pause
-
-Write-Output ""
-
-function DelOrNot () {
+function DelOrNot ($regExpression) {
                    $YesNo = Read-Host -Prompt "Do you want to delete all the duplicated files? (Y/N)"
 
                    if ( ($YesNo -eq "N") -or ($YesNo -eq "n") ) { Write-Output "You can now close the powershell." }
                    ElseIf ( ($YesNo -eq "Y") -or ($YesNo -eq "y") ) {
 
                            Get-ChildItem .\ -Recurse -File | 
-                           Where-Object { $_.Name -match ".*\([0-9]?[0-9]?[0-9]\)\.[A-z,0-9]{2,4}" } |
+                           Where-Object { $_.Name -match $regExpression } |
                            Remove-Item
     
                             Write-Output "All the duplicated files have been removed."
                           }
                     }
 
+function searchFile($regExpression) {
 foreach ($file in Get-ChildItem .\ -Recurse -File | 
-          Where-Object { $_.Name -match ".*\([0-9]?[0-9]?[0-9]\)\.[A-z,0-9]{2,4}" }) {
+          Where-Object { $_.Name -match $regExpression }) {
 
           Write-Output $file
         }
@@ -33,7 +44,9 @@ foreach ($file in Get-ChildItem .\ -Recurse -File |
              Write-Output ""
              Write-Output "You can now close the powershell."
            }
-           else { DelOrNot}
-
+           else { DelOrNot($regExpression)}
+}
 Write-Output ""
+askHost
+
 pause
