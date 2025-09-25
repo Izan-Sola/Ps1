a25fdab7571f8238832ef14673bee3ca781c06ab
Izan Sola Montesinos <83542827+Izan-Sola@users.noreply.github.com>
2025-09-25 08:52:59 +0200

    Add files via upload

--- DeleteDuplicatedFiles.ps1
+++ DeleteDuplicatedFiles.ps1
@@ -1,5 +1,5 @@
 
-Set-Location C:\Users\$env:UserName
+Set-Location C:\Users\2smra
 
 Write-Output "All the duplicated files with their directories are going to be shown."
 Write-Output ""
@@ -8,38 +8,32 @@ pause
 
 Write-Output ""
 
-function DelOrNot ()
-                  {
+function DelOrNot () {
                    $YesNo = Read-Host -Prompt "Do you want to delete all the duplicated files? (Y/N)"
 
-                   if ( ($YesNo -eq "N") -or ($YesNo -eq "n") )
-                      {
-                       Write-Output "You can now close the powershell."
-                      }
-                  
-                   ElseIf ( ($YesNo -eq "Y") -or ($YesNo -eq "y") )
-                          {
-                           Get-ChildItem .\ -Name "*(?)*.*" -Recurse -File | Remove-Item 
-                                 
-                           Write-Output ""
-                           Write-Output "All the duplicated files have been removed."
+                   if ( ($YesNo -eq "N") -or ($YesNo -eq "n") ) { Write-Output "You can now close the powershell." }
+                   ElseIf ( ($YesNo -eq "Y") -or ($YesNo -eq "y") ) {
+
+                           Get-ChildItem .\ -Recurse -File | 
+                           Where-Object { $_.Name -match ".*\([0-9]?[0-9]?[0-9]\)\.[A-z,0-9]{2,4}" } |
+                           Remove-Item
+    
+                            Write-Output "All the duplicated files have been removed."
                           }
                     }
 
-foreach ($file in Get-ChildItem .\ -Name "*(?)*.*" -Recurse -File)
-        {
-         Out-String -InputObject $file
+foreach ($file in Get-ChildItem .\ -Recurse -File | 
+          Where-Object { $_.Name -match ".*\([0-9]?[0-9]?[0-9]\)\.[A-z,0-9]{2,4}" }) {
+
+          Write-Output $file
         }
 
-         if([string]::IsNullOrEmpty($file))
-           {
+         if([string]::IsNullOrEmpty($file)) {
              Write-Output "No files have been found."
              Write-Output ""
              Write-Output "You can now close the powershell."
            }
-           else {
-                 DelOrNot 
-                }
+           else { DelOrNot}
 
 Write-Output ""
 pause
